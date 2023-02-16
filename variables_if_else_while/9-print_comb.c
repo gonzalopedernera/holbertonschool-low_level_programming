1e39acda69dfdb9820b3db4c0be1b7abf3e32b19
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-16 09:18:50 -0800

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+
+for (ch = 48; ch <= 57; ch++)
+putchar(ch);
+if (ch >= 48);
+putchar(',');
+
+putchar('\n');
+return (0);
+}
