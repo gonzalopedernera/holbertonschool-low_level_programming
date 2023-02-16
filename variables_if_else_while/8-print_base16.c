5e5f98214dcdd6755653ccb65816197fc8b47567
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-16 09:10:34 -0800

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,9 +9,9 @@ int main(void)
 {
 int ch;
 
-for (ch = 97; ch <= 122; ch++)
-putchar(ch);
 for (ch = 48; ch <= 57; ch++)
+putchar(ch);
+for (ch = 97; ch <= 102; ch++)
 putchar(ch);
 
 putchar('\n');
