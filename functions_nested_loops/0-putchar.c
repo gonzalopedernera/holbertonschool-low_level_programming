24936b906b2c0bfac7b61f3ae389cb72e2d015de
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-17 05:27:18 -0800

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,28 @@
+#include <unistd.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
+ * _putchar @c: char
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+
+int _putchar(char c)
+
 {
-printf("_putchar\n");
-return (0);
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (write(1, &c, 1));
 }
+
+
+
+
