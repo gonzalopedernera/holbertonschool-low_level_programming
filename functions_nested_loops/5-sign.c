9f00887dce6218b0896d8b88271ed82074f39a65
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-17 13:06:39 -0800

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 if (n < 0)
 {
-printf("-");
+write(1, "-", 1);
 return (-1);
 }
 else if (n > 0)
 {
+write(1, "+", 1);
 return (1);
-putchar('+');
 }
 else
 {
+write(1, "0", 1);
 return (0);
-putchar('0');
 }
 }
