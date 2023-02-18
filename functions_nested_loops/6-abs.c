8d89a54b476a6825046acb4322f69eec32b80961
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-18 06:17:29 -0800

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - prints the absolute value of a number
+ *
+ * Return: returns the absolute value of a number
+ */
+
+int _abs(int r)
+{
+if (r < 0)
+return r * -1;
+else
+return r;
+}
