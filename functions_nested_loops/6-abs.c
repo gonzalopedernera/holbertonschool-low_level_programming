e6a86f6e2f74c2ac989b001ea7809b0095087fc2
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-18 06:20:17 -0800

    6-abs.c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,13 +2,15 @@
 /**
  * _abs - prints the absolute value of a number
  *
+ * @r: a number
+ *
  * Return: returns the absolute value of a number
  */
 
 int _abs(int r)
 {
 if (r < 0)
-return r * -1;
+return( r * -1);
 else
-return r;
+return(r);
 }
