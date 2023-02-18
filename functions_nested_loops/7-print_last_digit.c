c8ea1480e0b7fcda007b24f4c8ba19074b049135
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-18 10:43:02 -0800

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -11,15 +11,15 @@
 
 int print_last_digit(int num)
 {
-int lastdig;
+ int lastdig;
 
-lastdig = num % 10;
+ lastdig = num % 10;
 
-if(lastdig < 0)
-{
-lastdig = (lastdig * -1);
-}
+ if(lastdig < 0)
+  {
+    lastdig = (lastdig * -1);
+  }
 
-putchar(lastdig + '0');
-return (lastdig);
+ putchar(lastdig + '0');
+ return (lastdig);
 }
