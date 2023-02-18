5f9996d13a93165a2b44144dd5137dab98ef3841
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-18 10:02:31 -0800

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ *
+ * @a: a number
+ *
+ * @b: a number
+ *
+ * return: the result of a + b
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
