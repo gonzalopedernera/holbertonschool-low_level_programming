6ff7dc72817455efb3bd07ed0d44386f504b9a4e
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-22 12:08:33 -0800

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - prints '_' a number of times equal to n
+ *
+ * @n: a number
+ */
+void print_line(int n)
+{
+if (n > 0)
+{
+	_putchar('_' * n);
+	_putchar('\n');
+}
+if (n <= 0)
+	_putchar('\n');
+}
+
