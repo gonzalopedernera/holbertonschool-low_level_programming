94c72c85663ead844f2fc02b8ccf618200a96ba1
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-22 10:42:42 -0800

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - prints all numbers from 0 to 14 ten times
+ */
+void more_numbers(void)
+{
+	int i;
+
+	int num;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (num = 0; num <= 14; num++)
+			_putchar(num + '0');
+	}
+	_putchar ('\n');
+}
+
