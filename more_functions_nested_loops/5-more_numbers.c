7b3c4c24dd7cc0958b1762464fce2fd34f2a035f
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-22 11:10:55 -0800

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,12 +6,16 @@ void more_numbers(void)
 {
 	int i;
 
-	int num;
+	int num = 0;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
+			if (num >= 10)
+			{
+				_putchar((num / 10));
+			}
 			_putchar(num + '0');
 		}
 		_putchar('\n');
