f7157cd4fed3501124ed16425a29c3d15ed2376d
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-23 05:14:13 -0800

    8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -10,17 +10,16 @@ void print_square(int size)
 int i;
 int j;
 
-if (size <= 0)
-{
-	for (i = 0; i <= size; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j <= size; j++)
+		for (j = 1; j <= size; j++)
 		{
 			_putchar(35);
 		}
 		_putchar('\n');
 	}
+
+if (size <= 0)
 	_putchar('\n');
-}
 }
 
