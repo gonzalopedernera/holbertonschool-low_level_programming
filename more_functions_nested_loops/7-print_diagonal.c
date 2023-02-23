09dc4f7b9cfb231bbb3833652f2042e31583b429
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-23 05:32:54 -0800

    8-print_square.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,16 +6,19 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+int i;
+int j;
 
-	if (n >= 0)
-	{
-	for (i = 0; i < n; i++)
-	{
-		_putchar(127);
-	}
-		_putchar(92);
-	}
-		_putchar('\n');
+for (i = 1; i < n; i++)
+{
+for (j = 1; j < n; j++)
+{
+	_putchar(127);
+}
+	_putchar(92);
+	_putchar('\n');
+}
+if (n <= 0)
+	_putchar('\n');
 }
 
