00046e9e393c182766b85033392206ed688defdc
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-27 11:06:58 -0800

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverses a string
+ *
+ * @s: a string
+ */
+void rev_string(char *s)
+{
+	int i;
+	int l = strlen(s);
+	char p[400];
+	int j = 0;
+
+	for (i = (l - 1); i >= 0; i--)
+	{
+		p[j] = s[i];
+		j++;
+	}
+	strcpy(s, p);
+}
+
