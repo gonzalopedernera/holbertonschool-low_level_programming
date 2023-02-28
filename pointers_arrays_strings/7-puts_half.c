b7acf738d15032a3d963d7b234f24103ce12ad99
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-28 06:22:01 -0800

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,3 +1,18 @@
 #include "main.h"
+#include <string.h>
 
+void puts_half(char *str)
+{
+	int i;
+	int l = strlen(str);
+
+	for (i = 0; i < l; i++)
+	{
+		if (i >= 5)
+		{
+			_putchar(str[i]);
+		}
+	}
+		_putchar('\n');
+}
 
