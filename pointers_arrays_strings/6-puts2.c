cff30ae1a4f4485728a166cfae02a99d8899dbb4
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-28 06:04:13 -0800

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * puts2 - prints every other character of a string
  *
@@ -7,8 +8,9 @@
 void puts2(char *str)
 {
 	int i;
+	int l = strlen(str);
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; i < l; i += 2)
 	{
 		_putchar(str[i]);
 	}
