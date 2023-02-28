4bfc0ee2fad098bf4d2c58648108035f6015f5b7
gonzalopedernera <gonzalopeder2@gmail.com>
2023-02-28 05:49:54 -0800

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,14 +6,11 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
-		if ((i % 2) == 0 && i == 0)
-		{
-			_putchar(str[i]);
-		}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
