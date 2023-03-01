b2bb4c07d43c5db6231ce3502b6355b7e5297cc7
gonzalopedernera <gonzalopeder2@gmail.com>
2023-03-01 09:25:02 -0800

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy - the same function as strncpy from the standard library
+ *
+ * @dest: destination of the copy
+ *
+ * @src: source of the copy
+ *
+ * @n: number of bytes to be copied
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
