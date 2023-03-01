ae3616a6e4d6622c760a72379cbaefa3f0991a17
gonzalopedernera <gonzalopeder2@gmail.com>
2023-03-01 10:12:58 -0800

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: string 1
+ *
+ * @s2: string 2
+ *
+ * Return: 0 if equal, positive if s1 > s2, negative if s1 < s2)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int r;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+	if (s1[i] == s2[i])
+	{
+		r = 0;
+	}
+
+	if (s1[i] > s2[i])
+	{
+		r = 1;
+	}
+
+	if (s1[i] < s2[i])
+	{
+		r = -1;
+	}
+	i++;
+	}
+	return (r);
+}
+
