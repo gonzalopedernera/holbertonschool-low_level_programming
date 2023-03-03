781657fb88fac8314ca48e7c7e9502706ebe3898
gonzalopedernera <gonzalopeder2@gmail.com>
2023-03-03 05:05:00 -0800

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _atoi - converts a string to an integer
+ *
+ * @s: pointer to a string
+ *
+ * Return: string converted
+ */
+int _atoi(char *s)
+{
+	int nc = 1;
+	unsigned int i = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+			nc *= -1;
+		}
+		else if (*s <= 0 && *s >= 9)
+		{
+			i = i * 10 + (*s - '0');
+		}
+		else if (i > 0)
+		{
+		break;
+		}
+		s++;
+	}
+	return (i * nc);
+}
