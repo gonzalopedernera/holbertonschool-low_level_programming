988fea0e7877687900e1b2254b4362f2752160b8
gonzalopedernera <gonzalopeder2@gmail.com>
2023-03-02 16:15:25 -0800

    2-strchr.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,5 +21,7 @@ char *string_toupper(char *p);
 char *cap_string(char *p);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 #endif
 
