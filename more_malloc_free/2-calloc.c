6268a96ee63cac9d3b0ab39c235a8035bbb53050
gonzalopedernera <gonzalopeder2@gmail.com>
2023-03-16 15:50:46 -0700

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,18 +12,25 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *s;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
 
-	size = nmemb;
+	size *= nmemb;
 
 	s = malloc(size);
 	if (s == NULL)
 	{
 		return (NULL);
 	}
+
+	for (i = 0; i < size; i++)
+	{
+		s[i] = 0;
+	}
+
 	return (s);
 }
