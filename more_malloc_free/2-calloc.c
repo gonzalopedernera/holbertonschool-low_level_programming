8dd2411c1b374a9e2190f6280020c3590291001d
gonzalopedernera <gonzalopeder2@gmail.com>
2023-03-16 15:40:37 -0700

    2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocates nmemb amount of memory using malloc
+ *
+ * @nmemb: max amount of memory to be allocated
+ *
+ * @size: size of var
+ *
+ * Return: pointer to allocated memory, or NULL if fail
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *s;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	size = nmemb;
+
+	s = malloc(size);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	return (s);
+}
