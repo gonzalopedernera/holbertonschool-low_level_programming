4b9f8730e01aec2501d273b1b2b7874238fc4e60
gonzalopedernera <gonzalopeder2@gmail.com>
2023-03-20 06:03:23 -0700

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef structdog
+#define structdog
+/**
+ * struct dog - struct for dog information
+ *
+ * @name: dog name
+ *
+ * @age: dog age
+ *
+ * @owner: dog owner name
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
