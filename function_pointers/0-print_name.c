a3df2f47fabae4d1de61eea5fd1e8279b87505f1
gonzalopedernera <gonzalopeder2@gmail.com>
2023-03-21 11:02:19 -0700

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - prints a name
  *
@@ -8,5 +9,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f != NULL)
+	{
 	(*f)(name);
+	}
 }
