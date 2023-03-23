f64ae0f5bc1c85278e0f1481c62c6ca6405d690a
gonzalopedernera <gonzalopeder2@gmail.com>
2023-03-23 13:57:10 -0700

    4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,81 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strcpy - copies a string
+ *
+ * @dest: destination of the copy
+ *
+ * @src: source of the copy
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+/**
+ * _strlen - calculates the length of a string
+ *
+ * @s: a string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0';)
+	{
+		++i;
+	}
+
+	return (i);
+}
+/**
+ * new_dog - creates a new dog
+ *
+ * @name: new dog name
+ *
+ * @age: new dog age
+ *
+ * @owner: new dog owner
+ *
+ * Return: pointer to new dog into, NULL if it fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *nd;
+
+	nd = malloc(sizeof(dog_t));
+	if (nd == NULL)
+	{
+		free(nd);
+		return (NULL);
+	}
+	nd->name = malloc(sizeof(char) * _strlen(name));
+	nd->owner = malloc(sizeof(char) * _strlen(owner));
+
+	if (nd->name == NULL || nd->owner == NULL)
+	{
+		free(nd->name);
+		free(nd->owner);
+		free(nd);
+		return (NULL);
+	}
+
+	nd->name = _strcpy(nd->name, name);
+	nd->age = age;
+	nd->owner = _strcpy(nd->owner, owner);
+	return (nd);
+
+}
