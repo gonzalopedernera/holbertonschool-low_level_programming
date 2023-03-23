a58d8a79c5f30639d8813a03ce8afc8dc00bddec
gonzalopedernera <gonzalopeder2@gmail.com>
2023-03-23 10:27:50 -0700

    3-main.c

---
+++ function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - performs simple operations
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: result of operation, or error if fail
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, res;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b  == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	res = op_func(a, b);
+	printf("%d\n", res);
+	return (0);
+}
