1bd866bc54cf9d35e25a770c61497388f38281a3
gonzalopedernera <gonzalopeder2@gmail.com>
2023-04-03 14:27:52 -0700

    task 2 complete and betty style correction

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strlen - calculates the length of a string
+ *
+ * @s: a string
+ *
+ * Return: length of the string
+ */
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0';)
+	{
+		++i;
+	}
+
+	return (i);
+}
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ *
+ * @head: begining of the list
+ *
+ * @str: content of the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+
+	newnode = malloc(sizeof(list_t));
+	newnode->str = strdup(str);
+
+	newnode->next = *head;
+	*head = newnode;
+	newnode->len = _strlen(str);
+	}
+	return (newnode);
+}
