c64624157c0e65d50d24db88f04e98dc0e389a34
gonzalopedernera <gonzalopeder2@gmail.com>
2023-04-04 11:41:01 -0700

    header betty style correction and standard libraries included

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,5 +1,8 @@
-#ifndef "LISTS_H"
-#define "LISTS_H"
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -11,9 +14,9 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
