50aa91f9090aee6a114a253d1e346897fc678d83
gonzalopedernera <gonzalopeder2@gmail.com>
2023-04-04 12:39:52 -0700

    task 4 completed, working and betty style corrected

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t list
+ *
+ * @head: the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
