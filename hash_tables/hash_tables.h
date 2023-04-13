d19871f5625a147fce7fc4bc00f45163d3063adb
gonzalopedernera <gonzalopeder2@gmail.com>
2023-04-13 15:03:32 -0700

    Header update. Added necessary libraries for memmory allocation

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,5 +1,6 @@
 #ifndef HASH_TABLES
 #define HASH_TABLES
+#include <stdlib.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -10,9 +11,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -25,8 +26,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
