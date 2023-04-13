98e4e8be82fd5aad57ad8a6d337d2f57d99c08e2
gonzalopedernera <gonzalopeder2@gmail.com>
2023-04-13 16:18:08 -0700

    Task 2 completed, working and Betty style corrected

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+ * key_index - gets the index of a key
+ *
+ * @key: the key
+ *
+ * @size: size of the array of the hash table
+ *
+ * Return: index of the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index, keyvalue;
+
+	keyvalue = hash_djb2(key);
+	index = keyvalue % size;
+
+	return (index);
+}
