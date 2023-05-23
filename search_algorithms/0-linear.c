9d8ef6ecc448103477e4e3fa2f1e83545193a30f
gonzalopedernera <gonzalopeder2@gmail.com>
2023-05-23 15:02:53 -0700

    Task 0 completed, working and Betty style corrected

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+/**
+ * linear_search - searches for a value in an array using Linear search algor
+ *
+ * @array: an array of integers.
+ *
+ * @size: size of the array.
+ *
+ * @value: value to search for
+ *
+ * Return: index where value is found or -1 if no array or no value in array
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i <= size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
+
